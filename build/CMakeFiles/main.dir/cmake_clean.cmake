b8731d33b6f220e35fdf439143d5edd8b52c7abc
Gutok66 <60787635+Gutok66@users.noreply.github.com>
2025-06-12 15:21:04 -0300

    Adicionado Template
    Laboratorio 5 adicionado como base para o desenvolvimento do trabalho

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/glad.c.obj"
+  "CMakeFiles/main.dir/src/glad.c.obj.d"
+  "CMakeFiles/main.dir/src/main.cpp.obj"
+  "CMakeFiles/main.dir/src/main.cpp.obj.d"
+  "CMakeFiles/main.dir/src/stb_image.cpp.obj"
+  "CMakeFiles/main.dir/src/stb_image.cpp.obj.d"
+  "CMakeFiles/main.dir/src/textrendering.cpp.obj"
+  "CMakeFiles/main.dir/src/textrendering.cpp.obj.d"
+  "CMakeFiles/main.dir/src/tiny_obj_loader.cpp.obj"
+  "CMakeFiles/main.dir/src/tiny_obj_loader.cpp.obj.d"
+  "G:/UFRGS/FCG/TrabalhoFinal/TrabalhoFinal-FCG/bin/Debug/main.exe"
+  "G:/UFRGS/FCG/TrabalhoFinal/TrabalhoFinal-FCG/bin/Debug/main.exe.manifest"
+  "G:/UFRGS/FCG/TrabalhoFinal/TrabalhoFinal-FCG/bin/Debug/main.pdb"
+  "libmain.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
