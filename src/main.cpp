d3e9e47243662e0d5838666c80511bf07ff56dab
bfzawacki <bfzawacki@inf.ufrgs.br>
2025-07-09 16:19:56 -0300

    nome do jogo

--- src/main.cpp
+++ src/main.cpp
@@ -655,7 +655,7 @@ int main(int argc, char* argv[])
     // Criamos uma janela do sistema operacional, com 800 colunas e 600 linhas
     // de pixels, e com título "INF01047 ...".
     GLFWwindow* window;
-    window = glfwCreateWindow(800, 600, "INF01047 - 00338627 - Augusto Kessler Pires e Bruno Fialho", NULL, NULL);
+    window = glfwCreateWindow(800, 600, "Strikepoint", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
